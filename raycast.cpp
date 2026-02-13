70bcac413cf07253d8d009f817dffdf6b71eac9f
Bobaliciousss <149013858+Bobaliciousss@users.noreply.github.com>
2026-02-12 17:09:13 -0700

    Progress on readscene; currently broken

--- raycast.cpp
+++ raycast.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <assert.h>
+#include <cstring>
+#include <string>
 #include "ppm.h"
 #include "v3math.h"
 
@@ -50,8 +52,46 @@ struct plane : shape {
 
 int readScene( char *sceneFileName, shape **objects ) {
 
+    printf("%s\n", sceneFileName);
+
     FILE *stream = fopen( sceneFileName, "r" );
     assert( stream != NULL );
+
+    char magicChars[12]; // Our "magic number" for the file
+    assert( fscanf( stream, "%s ", magicChars ) == 1 );
+    std::string sceneHeader = magicChars; // So I can compare it
+    std::string identityHeader = "img410scene";
+    if ( sceneHeader != identityHeader ) {
+        std::cerr << "Error: Invalid file format.\n";
+        return 1;
+    }
+
+    char tempObject[10];
+    std::string tempObjString = tempObject; // For comparison
+    std::string endOfScene = "end";
+
+    while ( tempObjString != endOfScene ) {
+
+        int outerResult = fscanf( stream, "%s ", tempObject );
+        assert( outerResult == 1 );
+        tempObjString = tempObject;
+        std::cout << tempObjString << "\n";
+
+        int c = fgetc( stream ); // Primer
+        char tempProperty[10];
+        std::string tempPropString = tempProperty;
+        while ( c != '\n' )
+        {
+
+            ungetc( c, stream );
+            fscanf( stream, "%s:", tempProperty );
+            std::cout << tempPropString;
+
+        }
+        
+    }
+
+    fclose( stream );
 
     return 0;
 
@@ -62,7 +102,7 @@ int main(int argc, char *argv[])
     
     if ( argc != 5 ) {
 
-        printf( "Usage: Blur width height input.scene output.ppm\n" );
+        printf( "Usage: raycast width height input.scene output.ppm\n" );
         return 1;
 
     }
@@ -70,8 +110,8 @@ int main(int argc, char *argv[])
 
         shape *objects;
 
-        if ( readScene( argv[4], &objects ) == 1 ) {
-
+        if ( readScene( argv[3], &objects ) == 1 ) {
+            return 1; // Invalid file format
         }
         else {
 
