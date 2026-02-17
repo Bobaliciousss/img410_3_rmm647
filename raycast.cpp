e472fd0700be09ede2e6bf2efc4b2967f5ebc3f4
Bobaliciousss <149013858+Bobaliciousss@users.noreply.github.com>
2026-02-16 17:08:36 -0700

    Completed read?

--- raycast.cpp
+++ raycast.cpp
@@ -12,8 +12,8 @@ extern "C" {
 
 struct camera {
 
-    int width;
-    int height;
+    float width;
+    float height;
 
 };
 
@@ -27,6 +27,12 @@ struct shape {
     virtual void intersect() {
         std::cerr << "Error: Cannot call intersect of base class \'Shape.\'\n";
     }
+    virtual void setRadius(  float radius ) {
+        std::cerr << "Error: Cannot assign radius property to base class \'Shape.\'\n";
+    }
+    virtual void setNormal(  float *normal ) {
+        std::cerr << "Error: Cannot assign normal property to base class \'Shape.\'\n";
+    }
 
 };
 
@@ -36,6 +42,10 @@ struct sphere : shape {
 
     void intersect(  ) {
         std::cout << "Placeholder: Calculate intersection for sphere.\n";
+    }
+    void setRadius(  float rds ) {
+        this->radius = rds;
+        std::cerr << "Notice: Assigned radius property to derived class \'Sphere.\'\n";
     }
 
 };
@@ -47,19 +57,23 @@ struct plane : shape {
     void intersect() {
         std::cout << "Placeholder: Calculate intersection for plane.\n";
     }
+    void setNormal(  float *nml ) {
+        this->normal = nml;
+        std::cerr << "Notice: Assigned normal property to derived class \'Plane.\'\n";
+    }
 
 };
 
-int readScene( char *sceneFileName, shape **objects ) {
+int readScene( char *sceneFileName, shape **objects, camera *camera, int *numberOfShapes ) {
 
     printf("\nReading %s\n\n", sceneFileName);
 
     FILE *stream = fopen( sceneFileName, "r" );
     assert( stream != NULL );
 
-    char magicChars[12]; // Our "magic number" for the file
+    char magicChars[12];
     assert( fscanf( stream, "%s ", magicChars ) == 1 );
-    std::string sceneHeader = magicChars; // So I can compare it
+    std::string sceneHeader = magicChars; // For comparison
     std::string identityHeader = "img410scene";
     if ( sceneHeader != identityHeader ) {
         std::cerr << "Error: Invalid file format.\n";
@@ -67,16 +81,22 @@ int readScene( char *sceneFileName, shape **objects ) {
     }
 
     char tempObject[10];
-    std::string tempObjString = tempObject; // For comparison
+    std::string tempObjString = tempObject;
     std::string endOfScene = "end";
+
     assert( fscanf( stream, "%s ", tempObject ) == 1 ); // Primer
     tempObjString = tempObject;
-
+    int objectsTableIndex = 0;
     // std::cout << tempObjString << "\n";
 
     while ( tempObjString != endOfScene ) {
 
-        
+        if ( tempObjString == "sphere" ) {
+            objects[ objectsTableIndex ] = new sphere;
+        }
+        else if ( tempObjString == "plane" ) {
+            objects[ objectsTableIndex ] = new plane;
+        }
 
         int c = fgetc( stream ); // Primer
         char tempProperty[10];
@@ -92,47 +112,69 @@ int readScene( char *sceneFileName, shape **objects ) {
             tempPropString = tempProperty;
             // std::cout << tempPropString << "\n";
 
-            float scannedFloat1;
-            float scannedFloat2;
-            float scannedFloat3;
             if ( tempPropString == "height:" ) {
-                fscanf( stream, "%f", &scannedFloat1 );
-                std::cout << "SCANNED HEIGHT\n";
+
+                assert( fscanf( stream, "%f", &( camera->height ) ) == 1 );
+                // std::cout << "SCANNED HEIGHT\n";
+
             }
             else if ( tempPropString == "width:" ) {
-                fscanf( stream, "%f", &scannedFloat1 );
-                std::cout << "SCANNED WIDTH\n";
+
+                assert( fscanf( stream, "%f", &( camera->width ) ) == 1 );
+                // std::cout << "SCANNED WIDTH\n";
+
             }
             else if ( tempPropString == "position:" ) {
-                fscanf( stream, "%f %f %f", &scannedFloat1, &scannedFloat2, &scannedFloat3 );
-                std::cout << "SCANNED POSITION\n";
+
+                float position[3] = { 0, 0, 0 };
+                fscanf( stream, "%f %f %f", &( position[0] ), &( position[1] ), &( position[2] ) );
+                // std::cout << "SCANNED POSITION\n";
+                objects[ objectsTableIndex ]->position = position;
+
             }
             else if ( tempPropString == "radius:" ) {
-                fscanf( stream, "%f", &scannedFloat1 );
-                std::cout << "SCANNED RADIUS\n";
+
+                float radius;
+                fscanf( stream, "%f", &radius );
+                // std::cout << "SCANNED RADIUS\n";
+                objects[ objectsTableIndex ]->setRadius( radius );
+                
             }
             else if ( tempPropString == "normal:" ) {
-                fscanf( stream, "%f %f %f", &scannedFloat1, &scannedFloat2, &scannedFloat3 );
-                std::cout << "SCANNED NORMAL\n";
+
+                float normal[3] = { 0, 0, 0 };
+                fscanf( stream, "%f %f %f", &( normal[0] ), &( normal[1] ), &( normal[2] ) );
+                // std::cout << "SCANNED NORMAL\n";
+                objects[ objectsTableIndex ]->setNormal( normal );
+                
+
             }
             else if ( tempPropString == "c_diff:" ) {
-                fscanf( stream, "%f %f %f", &scannedFloat1, &scannedFloat2, &scannedFloat3 );
-                std::cout << "SCANNED C_DIFF\n";
+
+                float cDiff[3] = { 0, 0, 0 };
+                fscanf( stream, "%f %f %f", &( cDiff[0] ), &( cDiff[1] ), &( cDiff[2] ) );
+                // std::cout << "SCANNED C_DIFF\n";
+                objects[ objectsTableIndex ]->cDiff = cDiff;
+
             }
 
             c = fgetc( stream );
 
         }
 
-        std::cout << "END OF LINE\n\n";
+        // std::cout << "END OF LINE\n\n";
 
         assert( fscanf( stream, "%s ", tempObject ) == 1 );
         tempObjString = tempObject;
         // std::cout << tempObjString << "\n";
+
+        objectsTableIndex ++;
         
     }
 
     fclose( stream );
+
+    *numberOfShapes = ( objectsTableIndex + 1 );
 
     return 0;
 
@@ -150,11 +192,17 @@ int main(int argc, char *argv[])
     else {
 
         shape *objects;
+        int numberOfShapes = 0;
+        camera camera;
 
-        if ( readScene( argv[3], &objects ) == 1 ) {
+        if ( readScene( argv[3], &objects, &camera, &numberOfShapes ) == 1 ) {
             return 1; // Invalid file format
         }
         else {
+
+            // TEMPORARY PRINT OBJECTS
+            std::cout << numberOfShapes << " shapes in array.\n\n";
+            //for ( int index=0; index< )
 
         }
 
